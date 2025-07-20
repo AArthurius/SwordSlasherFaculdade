24394acf6e94fe40f20f2b5761355fd091a4475c
AArthurius <arthurpennarey@gmail.com>
2025-07-20 13:59:42 -0300

    asdasd

--- Scripts/ranbow.gd
+++ Scripts/ranbow.gd
@@ -4,7 +4,7 @@ var tempo := 0.0
 
 func _process(delta):
 	tempo += delta * 1.5 # velocidade da animação
-
+	var teste = 10
 	# Gera cor com base no tempo usando HSV (Hue, Saturation, Value)
 	var cor = Color.from_hsv(fmod(tempo, 1.0), 1.0, 1.0)
 	self.modulate = cor
